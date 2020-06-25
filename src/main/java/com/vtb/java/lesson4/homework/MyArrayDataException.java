473de51dfd8e681634daf79952ff9b858067c6d9
vladder47 <vladisdrozdov@gmail.com>
2020-06-25 17:10:30 +0300

    Fourth homework added

---
+++ src/main/java/com/vtb/java/lesson4/homework/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package com.vtb.java.lesson4.homework;
+
+public class MyArrayDataException extends RuntimeException {
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
