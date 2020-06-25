41caa7f4fb8ec736d8e04b99f578a1657a93768c
vladder47 <vladisdrozdov@gmail.com>
2020-06-25 17:16:04 +0300

    Fourth homework added

---
+++ src/main/java/com/vtb/java/lesson4/homework/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package com.vtb.java.lesson4.homework;
+
+public class MyArraySizeException extends RuntimeException {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
