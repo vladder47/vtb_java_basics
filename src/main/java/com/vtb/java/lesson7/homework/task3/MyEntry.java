dae177a9daead48d0162a48257f902a987e4dca7
vladder47 <vladisdrozdov@gmail.com>
2020-07-02 15:44:00 +0300

    Seventh homework added

---
+++ src/main/java/com/vtb/java/lesson7/homework/task3/MyEntry.java
@@ -0,0 +1,19 @@
+package com.vtb.java.lesson7.homework.task3;
+
+public class MyEntry {
+    private Integer key;
+    private Integer value;
+
+    public MyEntry(Integer key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
